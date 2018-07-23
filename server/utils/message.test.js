69618a25efca57257f96cf84bf685558f049e272
unknown <navin2781mah@gmail.com>
2018-07-23 13:11:31 +0530

    adding test case , geolocation , to chat app

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -11,4 +11,17 @@ describe('generateMessage', () => {
     expect(message.createdAt).toBeA('number');
     expect(message).toInclude({from, text});
   });
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate current location object', () => {
+         let from = 'Navin';
+         let latitude = 15;
+         let longitude =19;
+         let url = 'https://www.google.com/maps?q=15,19';
+        var message = generateLocationMessage(from ,latitude , longitude);
+
+        expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({from, url});
+    });
 });
