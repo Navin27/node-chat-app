884376bc2d757430fb14f1148732ee1daa8a055c
Navin Kumar <navin2781mah@gmail.com>
2018-08-18 10:32:24 +0530

    new features added in chat app

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -14,14 +14,14 @@ describe('generateMessage', () => {
 });
 
 describe('generateLocationMessage', () => {
-    it('should generate current location object', () => {
-         let from = 'Navin';
-         let latitude = 15;
-         let longitude =19;
-         let url = 'https://www.google.com/maps?q=15,19';
-        var message = generateLocationMessage(from ,latitude , longitude);
+  it('should generate correct location object', () => {
+    var from = 'Deb';
+    var latitude = 15;
+    var longitude = 19;
+    var url = 'https://www.google.com/maps?q=15,19';
+    var message = generateLocationMessage(from, latitude, longitude);
 
-        expect(message.createdAt).toBeA('number');
-        expect(message).toInclude({from, url});
-    });
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, url});
+  });
 });
